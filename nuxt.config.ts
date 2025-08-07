376625392c341b7e24caec15096e9a3622db8f34
GBKS <chri@sto.ph>
2025-08-07 09:37:21 +0200

    Trying to trick bots into updating the share link preview image
    Let's see if a unique image URL does it.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,10 +41,10 @@ export default defineNuxtConfig({
   
         { property: 'og:url', content: 'https://event.bitcoin.design' },
         { property: 'og:site_name', content: 'Presidio Bitcoin Design Week' },
-        { property: 'og:image', content: 'https://event.bitcoin.design/preview.jpg' },
+        { property: 'og:image', content: 'https://event.bitcoin.design/preview-presidio.jpg' },
   
         { name: 'twitter:card', content: 'summary_large_image' },
-        { property: 'twitter:image', content: 'https://event.bitcoin.design/preview.jpg' },
+        { property: 'twitter:image', content: 'https://event.bitcoin.design/preview-presidio.jpg' },
         { property: 'twitter:title', content: 'Presidio Bitcoin Design Week' },
         { name: 'twitter:site', content: '@bitcoin_design' },
 
